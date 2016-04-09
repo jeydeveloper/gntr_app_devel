e7970cbbbdbf40f1baa2b3a4cb4412c609d3da7a
andy <juliadi@femaledaily.com>
2016-04-09 13:23:25 +0700

    update database

--- gntr_app.sql
+++ gntr_app.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Inang: 127.0.0.1
--- Waktu pembuatan: 04 Apr 2016 pada 21.44
+-- Waktu pembuatan: 09 Apr 2016 pada 13.22
 -- Versi Server: 5.5.32
 -- Versi PHP: 5.4.16
 
@@ -31,12 +31,22 @@ USE `gntr_app`;
 CREATE TABLE IF NOT EXISTS `gntrapp_adminuserlevels` (
   `aulv_id` int(11) NOT NULL AUTO_INCREMENT,
   `aulv_name` varchar(100) CHARACTER SET latin1 NOT NULL,
+  `aulv_void` tinyint(4) NOT NULL,
   `aulv_entryuser` varchar(100) CHARACTER SET latin1 NOT NULL,
   `aulv_entrydate` datetime NOT NULL,
   `aulv_changeuser` varchar(100) CHARACTER SET latin1 NOT NULL,
   `aulv_changedate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`aulv_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+
+--
+-- Dumping data untuk tabel `gntrapp_adminuserlevels`
+--
+
+INSERT INTO `gntrapp_adminuserlevels` (`aulv_id`, `aulv_name`, `aulv_void`, `aulv_entryuser`, `aulv_entrydate`, `aulv_changeuser`, `aulv_changedate`) VALUES
+(1, 'admin', 0, '', '0000-00-00 00:00:00', '', '2016-04-09 05:28:23'),
+(2, 'supervisor', 0, '', '0000-00-00 00:00:00', '', '2016-04-09 05:29:32'),
+(5, 'Coba', 0, '', '2016-04-09 13:17:55', '', '2016-04-09 06:17:55');
 
 -- --------------------------------------------------------
 
@@ -55,7 +65,14 @@ CREATE TABLE IF NOT EXISTS `gntrapp_adminusers` (
   `admusr_changeuser` varchar(100) NOT NULL,
   `admusr_changedate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`admusr_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data untuk tabel `gntrapp_adminusers`
+--
+
+INSERT INTO `gntrapp_adminusers` (`admusr_id`, `admusr_username`, `admusr_userpasswd`, `admusr_aulv_id`, `admusr_user_status`, `admusr_entryuser`, `admusr_entrydate`, `admusr_changeuser`, `admusr_changedate`) VALUES
+(1, 'superadmin', 'ac43724f16e9241d990427ab7c8f4228', NULL, 'y', '', '0000-00-00 00:00:00', '', '2016-04-09 05:27:29');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
