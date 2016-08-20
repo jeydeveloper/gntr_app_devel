cc99d0ebbc445610963c10dfcdd3be9a2ed985c9
andy <juliadi@femaledaily.com>
2016-08-20 12:11:04 +0700

    update barangjasa

--- application/modules_frontend/frontend_barangjasa/views/content.php
+++ application/modules_frontend/frontend_barangjasa/views/content.php
@@ -19,8 +19,6 @@
                     <th width="30"> No. </th>
                     <th> Nama </th>
                     <th> Kategori </th>
-                    <th> Jenis </th>
-                    <th> Volume </th>
                     <th> Satuan </th>
                     <th> Harga Satuan </th>
                     <th> Over Project </th>
@@ -35,11 +33,9 @@
                       <td><?php echo ($key+1); ?></td>
                       <td><?php echo $value['brjs_nama']; ?></td>
                       <td><?php echo (!empty($static_data_source['barjas_kategori'][$value['brjs_kategori_id']]) ? $static_data_source['barjas_kategori'][$value['brjs_kategori_id']]['name'] : '-'); ?></td>
-                      <td><?php echo (!empty($static_data_source['barjas_jenis'][$value['brjs_jenis_id']]) ? $static_data_source['barjas_jenis'][$value['brjs_jenis_id']]['name'] : '-'); ?></td>
                       <td><?php echo $value['brjs_volume']; ?></td>
-                      <td><?php echo (!empty($static_data_source['barjas_satuan'][$value['brjs_satuan_id']]) ? $static_data_source['barjas_satuan'][$value['brjs_satuan_id']]['name'] : '-'); ?></td>
-                      <td><?php echo $value['brjs_harga_satuan']; ?></td>
-                      <td><?php echo $value['brjs_over_project']; ?></td>
+                      <td><?php echo add_numberformat($value['brjs_harga_satuan']); ?></td>
+                      <td><?php echo (!empty($value['brjs_over_project']) ? 'Ya' : 'Tidak'); ?></td>
                       <td><?php echo (!empty($value['vndr_nama']) ? $value['vndr_nama'] : '-'); ?></td>
                       <td class="td-actions"><a href="<?php echo ($module_base_url.'/edit/'.$value['brjs_id']); ?>" class="btn btn-small btn-success" title="edit"><i class="btn-icon-only icon-pencil"> </i></a> <a href="<?php echo ($module_base_url.'/delete/'.$value['brjs_id']); ?>" class="btn btn-danger btn-small" title="delete"><i class="btn-icon-only icon-remove"> </i></a></td>
                     </tr>
