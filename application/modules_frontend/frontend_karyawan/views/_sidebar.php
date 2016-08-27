b0e66847607713d08d3c0a051e6cddd16f2f1d4f
andy <juliadi@femaledaily.com>
2016-08-27 12:21:30 +0700

    update role access

--- application/modules_frontend/frontend_karyawan/views/_sidebar.php
+++ application/modules_frontend/frontend_karyawan/views/_sidebar.php
@@ -11,12 +11,14 @@
       </a>
     </div>
     <!-- /shortcuts -->
+    <?php if(($this->session->userdata('userid') == 1) OR (!empty($role_access['karyawan']['create']))): ?>
     <div class="shortcuts"> 
       <a href="<?php echo site_url('karyawan/add'); ?>" class="shortcut" style="width: 100%;">
         <i class="shortcut-icon icon-file"></i>
         <span class="shortcut-label">New</span> 
       </a>
     </div>
+    <?php endif; ?>
     <!-- /shortcuts -->
     <div class="shortcuts"> 
       <a href="<?php echo site_url('karyawan/absen'); ?>" class="shortcut" style="width: 100%;">
