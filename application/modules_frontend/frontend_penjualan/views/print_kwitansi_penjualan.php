f6ead499f054bc8a03ab9e5e979caf3901b0e988
andy <juliadi@femaledaily.com>
2016-11-11 22:31:48 +0700

    update penjualan

--- application/modules_frontend/frontend_penjualan/views/print_kwitansi_penjualan.php
+++ application/modules_frontend/frontend_penjualan/views/print_kwitansi_penjualan.php
@@ -250,8 +250,8 @@
 <table style="width:100%;" border="0" cellspacing="0" cellpadding="0">
         <tr>
             <td style="width:50%; font-style: italic;">&nbsp;</td>
-            <td style="width:25%; font-style: italic;">&nbsp;</td>
-           <td style="width:25%;">
+            <td style="width:20%; font-style: italic;">&nbsp;</td>
+           <td style="width:30%;">
                 <table style="width: 100%" border="0" cellspacing="0" cellpadding="0">
                     <tr>
                     <?php
