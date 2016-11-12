3797895aeb14de885c1a427948591b6e5568269a
andy <juliadi@femaledaily.com>
2016-11-12 12:02:49 +0700

    update penjualan

--- application/modules_frontend/frontend_penjualan/views/kwitansi.php
+++ application/modules_frontend/frontend_penjualan/views/kwitansi.php
@@ -30,7 +30,7 @@
                     <tr>
                       <td><?php echo ($key+1); ?></td>
                       <td><?php echo $value['clnt_nama']; ?></td>
-                      <td><?php echo add_numberformat($value['nilai_pekerjaan'][$value['pjkw_ppnw_id']]); ?>x</td>
+                      <td><?php echo add_numberformat($value['nilai_pekerjaan'][$value['pjkw_ppnw_id']]); ?></td>
                       <td><?php echo (!empty($static_data_source['bank'][$value['pjkw_bank']]) ? $static_data_source['bank'][$value['pjkw_bank']]['name'] : '-'); ?></td>
                       <td><a href="<?php echo ($module_base_url_kwitansi.'/pdf/'.$value['pjkw_id']); ?>" target="_blank">View</a></td>
                       <td class="td-actions"><a href="<?php echo ($module_base_url_kwitansi.'/edit/'.$value['pjkw_id']); ?>" class="btn btn-small btn-success" title="edit"><i class="btn-icon-only icon-pencil"> </i></a> <a href="<?php echo ($module_base_url_kwitansi.'/delete/'.$value['pjkw_id']); ?>" class="btn btn-danger btn-small" title="delete"><i class="btn-icon-only icon-remove"> </i></a></td>
